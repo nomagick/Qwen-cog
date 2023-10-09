03a80c5426db5a60a6def67a73e3e2c976f92e72
Yanlong Wang <nomagick@naiver.org>
2023-10-09 11:58:57 +0800

    fix

--- patch_qwen.py
+++ patch_qwen.py
@@ -42,10 +42,14 @@ def chat_stream_raw(
         tokenizer,
         query,
         max_window_size=max_window_size,
-        chat_format='raw',
+        chat_format="raw",
     )
 
-    stop_words_ids.extend(qwen_generation_utils.get_stop_words_ids(generation_config.chat_format, tokenizer))
+    stop_words_ids.extend(
+        qwen_generation_utils.get_stop_words_ids(
+            generation_config.chat_format, tokenizer
+        )
+    )
     if stop_words_ids is not None:
         stop_words_logits_processor = qwen_generation_utils.StopWordsLogitsProcessor(
             stop_words_ids=stop_words_ids,
@@ -62,7 +66,7 @@ def chat_stream_raw(
     )
 
     outputs = []
-    last_response = ''
+    last_response = ""
     offset = len(last_response)
     for token in self.generate_stream(
         input_ids,
@@ -73,8 +77,10 @@ def chat_stream_raw(
         **kwargs,
     ):
         outputs.append(token.item())
-        last_response = tokenizer.decode(outputs, skip_special_tokens=True, errors="ignore")
-        if (last_response and last_response[-1] != '�'):
+        last_response = tokenizer.decode(
+            outputs, skip_special_tokens=True, errors="ignore"
+        )
+        if last_response and last_response[-1] != "�":
             chunk = last_response[offset:]
             offset = len(last_response)
             if chunk:
@@ -88,23 +94,20 @@ def patch(model):
 
     return model
 
+
 def _device_map(num_gpus, num_layers):
     per_gpu_layers = (num_layers + 2) / num_gpus
 
-    device_map = {
-        'transformer.wte': 0,
-        'transformer.ln_f': 0,
-        'lm_head': num_gpus-1
-    }
+    device_map = {"transformer.wte": 0, "transformer.ln_f": 0, "lm_head": num_gpus - 1}
 
     used = 1
     gpu_target = 0
     for i in range(num_layers):
         if used >= per_gpu_layers:
             gpu_target += 1
-            used = 0 if gpu_target < num_gpus-1 else 1
+            used = 0 if gpu_target < num_gpus - 1 else 1
         assert gpu_target < num_gpus
-        device_map[f'transformer.h.{i}'] = gpu_target
+        device_map[f"transformer.h.{i}"] = gpu_target
         used += 1
 
     return device_map
@@ -114,22 +117,38 @@ def load_model_on_gpus(model_name_or_path):
     num_devices = torch.cuda.device_count()
     bf16_supported = torch.cuda.is_bf16_supported()
 
-    if num_devices <= 1:
-        model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map='auto',
-                                                     use_flash_attn=bf16_supported,
-                                                     trust_remote_code=True)
+    if num_devices < 1:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path,
+            use_flash_attn=bf16_supported,
+            trust_remote_code=True,
+            local_files_only=True,
+        )
         model = patch(model).eval()
+    elif num_devices == 1:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path,
+            use_flash_attn=bf16_supported,
+            trust_remote_code=True,
+            local_files_only=True,
+        )
+        model = patch(model).cuda().eval()
     elif num_devices:
-        device_map = _device_map(num_devices, 40 if '14B' in model_name_or_path else 32)
-        model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map=device_map,
-                                                     use_flash_attn=bf16_supported,
-                                                     trust_remote_code=True)
+        device_map = _device_map(num_devices, 40 if "14B" in model_name_or_path else 32)
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path,
+            device_map=device_map,
+            use_flash_attn=bf16_supported,
+            trust_remote_code=True,
+            local_files_only=True,
+        )
         model = patch(model).eval()
         # model = dispatch_model(model, device_map=device_map)
     else:
         raise KeyError
-    
-    model.generation_config = GenerationConfig.from_pretrained(model_name_or_path, trust_remote_code=True)
 
+    model.generation_config = GenerationConfig.from_pretrained(
+        model_name_or_path, trust_remote_code=True
+    )
 
     return model
